2d0a4360b0cf4162696204885e4366cd0c015630
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-08-01 16:08:55 -0700

    add a new react project with simple app component

---
+++ index.android.js
@@ -0,0 +1,13 @@
+//ANDROID
+
+//Import a library to help create a component
+import React from 'react';
+import ReactNative from 'reactNative';
+
+//Create a component
+const App = () => {
+  return (
+    <Text>Some Text</Text>
+  );
+};
+//Render component to the device
