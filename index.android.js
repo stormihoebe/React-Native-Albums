4a238c7c042a092c0a3de979734aabe154b116e5
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-08-02 11:29:52 -0700

    add header to index

--- index.android.js
+++ index.android.js
@@ -2,12 +2,12 @@
 
 //Import a library to help create a component
 import React from 'react';
-import {Text, AppRegistry} from 'react-native';
-
+import { AppRegistry} from 'react-native';
+import Header from './src/components/header';
 
 //Create a component-- a javascript function that returns jsx
 const App = () => (
-  <Text>Some Text</Text>
+    <Header />
 );
 //Render component to the device
 AppRegistry.registerComponent('albums', () => App);
