23083bd77d78427de1ab5be0abe3505099435d15
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-08-02 15:35:33 -0700

    add style and touchopacity to button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,35 @@
 //Import libraries for making a component
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, TouchableOpacity} from 'react-native';
 
 //create the component
 const Button = () => {
 
   return (
-    <Text>Click</Text>
+    <TouchableOpacity style={styles.buttonStyle}>
+      <Text style={styles.textStyle}>Click</Text>
+    </TouchableOpacity>
   );
 }
 const styles = {
+  buttonStyle: {
+    flex: 1,
+    alignSelf: 'stretch',
+    backgroundColor: '#fff',
+    borderWidth:1,
+    borderRadius: 4,
+    borderColor: '#007aff',
+    marginLeft: 5,
+    marginRight: 5
+  },
+  textStyle:{
+    alignSelf: 'center',
+    color: '#007aff',
+    fontSize: 16,
+    fontWeight: '600',
+    paddingTop: 10,
+    paddingBottom: 10
+  }
 
 }
 //make the new component available to the other parts of the App
