1f9bcc575cf47059be2243b3cbb562f3319efa1a
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-08-02 11:13:47 -0700

    add text to header component and export

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+//Import libraries for making a component
+import React from 'react';
+import {Text} from 'react-native';
+
+//create the component
+const Header = () => {
+  return <Text>Albums!</Text>;
+}
+
+//make the new component available to the other parts of the App
+export default Header;
