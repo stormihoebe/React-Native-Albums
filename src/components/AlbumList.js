fa82300a4cb3c4e20b72a170d3b7b123fb3b6bf6
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-08-02 13:36:54 -0700

    add state to album list class component

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,9 +1,22 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component{
+  state= {
+    albums: []
+  };
+
+  componentWillMount(){
+    fetch('https://rallycoding.herokuapp.com/api/music_albums')
+    .then((response)=> response.json())
+    .then((responseData)=> {
+      console.log(responseData);
+    });
+  }
 
   render(){
+
     return (
       <View>
         <Text>Heyooo</Text>
