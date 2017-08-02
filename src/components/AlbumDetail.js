e20145c8dbbc40a2d5e8830c8dc0241b74e961c9
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-08-02 15:10:36 -0700

    add additional styles to album detail

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -4,18 +4,22 @@ import Card from './Card';
 import CardSection from './CardSection';
 
 const AlbumDetail = ({album}) => {
-  const {title, artist, thumbnail_image} = album;
+  const {title, artist, thumbnail_image, image} = album;
 
   return (
     <Card>
       <CardSection>
-        <View>
+        <View style={styles.thumbnailContainerStyle}>
           <Image style={styles.thumbnailStyle} source={{uri: thumbnail_image}}></Image>
         </View>
         <View style={styles.headerContentStyle}>
-          <Text>{title}</Text>
+          <Text style={styles.headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
+      </CardSection>
+      <CardSection>
+        <Image style={styles.imageStyle} source={{uri: image}}></Image>
+
       </CardSection>
     </Card>
   );
@@ -26,10 +30,24 @@ const styles = {
     flexDirection: 'column',
     justifyContent: 'space-around',
   },
+  headerTextStyle: {
+    fontSize:18,
+  },
   thumbnailStyle: {
     height: 60,
     width: 60
-  }
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 5,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  },
 }
 
 export default AlbumDetail;
