e7c9b5882f143d1d5231de818a5317b99958d75f
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-08-02 14:42:26 -0700

    add style and view tag to cardSection component

--- src/components/CardSection.js
+++ src/components/CardSection.js
@@ -3,14 +3,23 @@ import {View} from 'react-native';
 
 const CardSection = (props) => {
   return (
-    <View>
+    <View style={styles.containerStyle}>
+      {props.children}
     </View>
   );
 };
 
 const styles = {
-
+  containerStyle: {
+    borderBottomWidth: 1,
+    padding: 5,
+    backgroundColor: '#fff',
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+    borderColor: '#ddd',
+    position: 'relative'
   }
 }
+
 
 export default CardSection;
